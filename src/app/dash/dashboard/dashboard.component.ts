05fa6d512c3b5c22c4f76edbdb665a5fdc3ca1ac
Brian Cusack <briancusack78@gmail.com>
2021-08-17 17:26:58 +1000

    wiki subscriber

--- src/app/dash/dashboard/dashboard.component.ts
+++ src/app/dash/dashboard/dashboard.component.ts
@@ -1,27 +1,32 @@
 
 import { Component, OnInit } from '@angular/core';
-
+import { from } from 'rxjs/internal/observable/from';
+import { of } from 'rxjs/internal/observable/of';
+import { map } from 'rxjs/internal/operators/map';
+import { mergeAll, mergeMap } from 'rxjs/operators';
 import { CatDataService } from 'src/app/shared/services/cat-data.service';
+import { WikiService } from 'src/app/shared/services/wiki.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  categories: string[] | undefined;
+export class DashboardComponent {
+  categories: string[];
+  cat: any;
 
-  constructor(private dataservice: CatDataService) {
+  constructor(private dataservice: CatDataService, private wikidata: WikiService) {
+    this.categories = this.dataservice.getAllCategories();
+    this.cat = this.getWikis(this.categories).subscribe(console.log);
 
   }
 
-  ngOnInit(): void {
-    // this.getAllData();
-    this.categories = this.getCategoriesList();
-  }
-
-  getCategoriesList() {
-    return this.dataservice.getAllCategories();
+  getWikis(cate: string[]) {
+    return from(cate)
+      .pipe(mergeMap((d) =>
+        this.wikidata.getWiki(d)
+      ))
   }
 
 
